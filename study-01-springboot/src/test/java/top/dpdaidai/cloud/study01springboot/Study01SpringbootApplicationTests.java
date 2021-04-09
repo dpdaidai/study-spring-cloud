946741f7c7da54cec70d973eb775d59f2e8c63f0
cpt at mac <pantaochen@gmail.com>
2021-04-09 23:44:47 +0800

    降低spring版本为1.3.7release

--- study-01-springboot/src/test/java/top/dpdaidai/cloud/study01springboot/Study01SpringbootApplicationTests.java
+++ study-01-springboot/src/test/java/top/dpdaidai/cloud/study01springboot/Study01SpringbootApplicationTests.java
@@ -2,31 +2,38 @@ package top.dpdaidai.cloud.study01springboot;
 
 
 import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import top.dpdaidai.cloud.study01springboot.web.HelloController;
 
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringApplicationConfiguration(classes = DemoApplication.class)
-//@WebAppConfiguration
-@SpringBootTest
-class Study01SpringbootApplicationTests {
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Study01SpringbootApplication.class)
+@WebAppConfiguration
+//@SpringBootTest
+public class Study01SpringbootApplicationTests {
 
     private static MockMvc mockMvc;
 
-    @BeforeAll
-    public static void setUp() {
+    //    @BeforeAll
+//    public static void setUp() {
+//        mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+//    }
+
+    @Before
+    public void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
     }
 
