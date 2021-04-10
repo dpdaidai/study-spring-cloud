562855073393df703687b992be9741475686046a
cpt at mac <pantaochen@gmail.com>
2021-04-10 14:50:24 +0800

    增加自定义统计指标信息

--- study-01-springboot/src/main/java/top/dpdaidai/cloud/study01springboot/web/HelloController.java
+++ study-01-springboot/src/main/java/top/dpdaidai/cloud/study01springboot/web/HelloController.java
@@ -1,5 +1,8 @@
 package top.dpdaidai.cloud.study01springboot.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+
+    @Qualifier("counterService")
+    @Autowired
+    CounterService counterService;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String index() {
+
+        //自定义统计指标信息
+        counterService.increment("top.dpdaidai.hello.count");
+
         return "hello boot";
     }
 }
