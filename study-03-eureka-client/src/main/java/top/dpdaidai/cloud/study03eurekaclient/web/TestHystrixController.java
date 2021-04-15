506d688e54c817d6b4dba97fb5660ef1d03d5184
cpt at mac <pantaochen@gmail.com>
2021-04-15 14:48:12 +0800

    测试Hystrix断路器的服务阻塞熔断效果

--- study-03-eureka-client/src/main/java/top/dpdaidai/cloud/study03eurekaclient/web/TestHystrixController.java
+++ study-03-eureka-client/src/main/java/top/dpdaidai/cloud/study03eurekaclient/web/TestHystrixController.java
@@ -2,11 +2,11 @@ package top.dpdaidai.cloud.study03eurekaclient.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 /**
  * @Author chenpantao
@@ -18,12 +18,14 @@ public class TestHystrixController {
 
     private static final Logger logger = LoggerFactory.getLogger(TestHystrixController.class);
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
     @RequestMapping(value = "/testHystrix", method = RequestMethod.GET)
-    public String testHystrix() {
-        return "hello";
+    public String testHystrix() throws InterruptedException {
+
+        //设置接口随机阻塞0-2秒 , 当阻塞大于一秒时 , 消费者会触发Hystrix的熔断请求
+        int i = new Random().nextInt(2000);
+        logger.info("sleepTime:{}", i);
+        Thread.sleep(i);
+        return "testHystrix";
     }
 
 
--- study-04-ribbon-consumer/src/main/java/top/dpdaidai/cloud/study04ribbonconsumer/web/HelloService.java
+++ study-04-ribbon-consumer/src/main/java/top/dpdaidai/cloud/study04ribbonconsumer/web/HelloService.java
@@ -1,6 +1,8 @@
 package top.dpdaidai.cloud.study04ribbonconsumer.web;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -20,18 +22,25 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
-    //断路器 , 当超过hystrix默认的两秒后 , 熔断请求 , 触发回调函数
+    private static final Logger logger = LoggerFactory.getLogger(HelloService.class);
+
+    //断路器 , 当超过hystrix默认的一秒后 , 熔断请求 , 触发回调函数
     //回调函数的参数必须与本方法一致 , 不然会报 FallbackDefinitionException
     @HystrixCommand(fallbackMethod = "helloFallBack")
     public String helloConsumer(HttpServletResponse response) {
+        logger.info("start");
+        long start = System.currentTimeMillis();
         ResponseEntity<String> forEntity = restTemplate.getForEntity("http://HELLO-SERVICE/testHystrix", String.class);
         HttpHeaders headers = forEntity.getHeaders();
-        System.out.println(headers.get("serviceId-host-post").get(0));
+        logger.info(headers.get("serviceId-host-post").get(0));
         response.addHeader("serviceId-host-post", headers.get("serviceId-host-post").get(0));
+        long end = System.currentTimeMillis();
+        logger.info("Spend time : {}", end - start);
         return forEntity.getBody();
     }
 
     public String helloFallBack(HttpServletResponse response) {
+        logger.info("Hystrix effect");
         return "error";
     }
 
