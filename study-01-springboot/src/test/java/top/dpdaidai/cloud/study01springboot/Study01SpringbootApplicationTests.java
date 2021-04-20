3c443d0084f6b3d35c34de68069c270b939c10f8
cpt at mac <pantaochen@gmail.com>
2021-04-20 16:03:08 +0800

    注释Test类 , 暂时不用

--- study-01-springboot/src/test/java/top/dpdaidai/cloud/study01springboot/Study01SpringbootApplicationTests.java
+++ study-01-springboot/src/test/java/top/dpdaidai/cloud/study01springboot/Study01SpringbootApplicationTests.java
@@ -1,47 +1,47 @@
-package top.dpdaidai.cloud.study01springboot;
-
-
-import org.hamcrest.CoreMatchers;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import top.dpdaidai.cloud.study01springboot.web.HelloController;
-
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Study01SpringbootApplication.class)
-@WebAppConfiguration
-//@SpringBootTest
-public class Study01SpringbootApplicationTests {
-
-    private static MockMvc mockMvc;
-
-    //    @BeforeAll
-//    public static void setUp() {
+//package top.dpdaidai.cloud.study01springboot;
+//
+//
+//import org.hamcrest.CoreMatchers;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//import top.dpdaidai.cloud.study01springboot.web.HelloController;
+//
+////import org.junit.jupiter.api.BeforeAll;
+////import org.junit.jupiter.api.Test;
+////import org.springframework.boot.test.context.SpringBootTest;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = Study01SpringbootApplication.class)
+//@WebAppConfiguration
+////@SpringBootTest
+//public class Study01SpringbootApplicationTests {
+//
+//    private static MockMvc mockMvc;
+//
+//    //    @BeforeAll
+////    public static void setUp() {
+////        mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+////    }
+//
+//    @Before
+//    public void setUp() {
 //        mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
 //    }
-
-    @Before
-    public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
-    }
-
-    @Test
-    public void hello() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.equalTo("hello boot")));
-    }
-
-}
+//
+//    @Test
+//    public void hello() throws Exception {
+//        mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpect(MockMvcResultMatchers.content().string(CoreMatchers.equalTo("hello boot")));
+//    }
+//
+//}
