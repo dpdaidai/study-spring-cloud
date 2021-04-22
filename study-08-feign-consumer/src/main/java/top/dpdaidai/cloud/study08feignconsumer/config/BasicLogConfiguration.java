ad2beb5afab47418802f124fa0666eebe3d1e242
cpt at mac <pantaochen@gmail.com>
2021-04-22 14:48:41 +0800

    1  在配置文件中指定feign client开启log日志 2  配置configuration设置log level 等级为FULL 3  configuration可以配置多个 , 意思应该是各个client可以输出不同等级的 log level . 但是我这里没能实现 , 初步猜测在 feign client 配置的等级其实是按照服务名绑定的 . 具体需要看源码 , 或者做测试

---
+++ study-08-feign-consumer/src/main/java/top/dpdaidai/cloud/study08feignconsumer/config/BasicLogConfiguration.java
@@ -0,0 +1,24 @@
+//package top.dpdaidai.cloud.study08feignconsumer.config;
+//
+//import feign.Logger;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// *
+// * 配置Feign客户端的是否打印log
+// *
+// * @Author chenpantao
+// * @Date 4/22/21 2:08 PM
+// * @Version 1.0
+// */
+//@Configuration
+//public class BasicLogConfiguration {
+//
+//    @Bean
+//    Logger.Level feignLevel(){
+//        return Logger.Level.BASIC;
+//    }
+//
+//
+//}
