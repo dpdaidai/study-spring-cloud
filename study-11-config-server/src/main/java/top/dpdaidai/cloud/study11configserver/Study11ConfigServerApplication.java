f570a86a6b77afdbbaeee2b55e5266166986af35
cpt at mac <pantaochen@gmail.com>
2021-04-25 18:47:27 +0800

    config 引入eureka服务治理 , 客户端根据注册中心的serviceId来加载配置文件

--- study-11-config-server/src/main/java/top/dpdaidai/cloud/study11configserver/Study11ConfigServerApplication.java
+++ study-11-config-server/src/main/java/top/dpdaidai/cloud/study11configserver/Study11ConfigServerApplication.java
@@ -2,11 +2,14 @@ package top.dpdaidai.cloud.study11configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 //开启SpringCloudConfig服务端功能
 @EnableConfigServer
+//开启微服务注册
+@EnableDiscoveryClient
 public class Study11ConfigServerApplication {
 
     public static void main(String[] args) {
--- study-12-config-client/src/main/java/top/dpdaidai/cloud/study12configclient/Study12ConfigClientApplication.java
+++ study-12-config-client/src/main/java/top/dpdaidai/cloud/study12configclient/Study12ConfigClientApplication.java
@@ -2,8 +2,10 @@ package top.dpdaidai.cloud.study12configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Study12ConfigClientApplication {
 
     public static void main(String[] args) {
