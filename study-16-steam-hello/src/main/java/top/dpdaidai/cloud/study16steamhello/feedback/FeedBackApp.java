ae1334fe68be025065843e51cd0f689c2298101e
cpt at mac <pantaochen@gmail.com>
2021-05-04 22:51:31 +0800

    stream 使用原生注解@ServiceActivator反馈消息

--- study-16-steam-hello/src/main/java/top/dpdaidai/cloud/study16steamhello/feedback/FeedBackApp.java
+++ study-16-steam-hello/src/main/java/top/dpdaidai/cloud/study16steamhello/feedback/FeedBackApp.java
@@ -3,13 +3,14 @@ package top.dpdaidai.cloud.study16steamhello.feedback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
 
 
 /**
  *
- * 接收消息 , 并使用@SendTo注解反馈消息
+ * 接收消息 , 并馈消息
  *
  * @Author chenpantao
  * @Date 5/4/21 10:04 PM
@@ -21,8 +22,22 @@ public class FeedBackApp {
 
     private static final Logger logger = LoggerFactory.getLogger(FeedBackApp.class);
 
-    @StreamListener(MyProcessor.INPUT)
-    @SendTo(MyProcessor.OUTPUT)
+    /**
+     * 使用@SendTo反馈消息
+     * @param payLoad
+     * @return
+     */
+//    @StreamListener(MyProcessor.INPUT)
+//    @SendTo(MyProcessor.OUTPUT)
+//    public Object receive(Object payLoad) {
+//        logger.info("receive my_input : {}", payLoad);
+//        return "From " + MyProcessor.INPUT + " channel : return - " + MyProcessor.OUTPUT;
+//    }
+
+    /**
+     * 使用原生注解@ServiceActivator反馈消息
+     */
+    @ServiceActivator(inputChannel = MyProcessor.INPUT, outputChannel = MyProcessor.OUTPUT)
     public Object receive(Object payLoad) {
         logger.info("receive my_input : {}", payLoad);
         return "From " + MyProcessor.INPUT + " channel : return - " + MyProcessor.OUTPUT;
