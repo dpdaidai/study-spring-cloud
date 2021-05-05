1c463bc302e5f5e2a29d22614247325ede6f53c5
cpt at mac <pantaochen@gmail.com>
2021-05-06 00:03:33 +0800

    sleuth 将zipkin收集消息的方式从 http接口方式 更换为 rabbitMQ

--- study-20-zipkin-server/src/main/java/top/dpdaidai/cloud/study20zipkinserver/Study20ZipkinServerApplication.java
+++ study-20-zipkin-server/src/main/java/top/dpdaidai/cloud/study20zipkinserver/Study20ZipkinServerApplication.java
@@ -2,10 +2,12 @@ package top.dpdaidai.cloud.study20zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinServer
+//@EnableZipkinServer
+@EnableZipkinStreamServer
 public class Study20ZipkinServerApplication {
 
     public static void main(String[] args) {
